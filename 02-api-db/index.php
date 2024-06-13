c51a23f17eb63f606e49d3649402dd69e437374c
Lucas Delobelle <lucas@ld-web.net>
2024-06-13 09:33:51 +0200

    :sparkles: CRUD courses, index.php

--- 02-api-db/index.php
+++ 02-api-db/index.php
@@ -8,6 +8,7 @@ set_exception_handler(function (Throwable $exception) {
     // Utiliser $exception pour créer un log d'erreur détaillé sur le serveur
     // Si $exception est quelque chose de grave, alors
     // envoyer un email à l'administrateur
+    var_dump($exception);
 
     http_response_code(500); // Internal Server Error
     echo json_encode([
@@ -75,7 +76,7 @@ if ($uri->getOperationType() === RequestUri::OPERATION_COLLECTION && $uri->getRe
     $id = $pdo->lastInsertId();
 
     echo json_encode([
-        'uri' => 'course.php?id=' . $id,
+        'uri' => '/courses/' . $id,
         'id_course' => $id,
         'course_name' => $arrayContent['name'],
         'cover_img_url' => $arrayContent['img'],
@@ -85,7 +86,7 @@ if ($uri->getOperationType() === RequestUri::OPERATION_COLLECTION && $uri->getRe
     exit;
 }
 
-// --- Récupération d'un cours seul -----------------------------------
+// --- Récupération d'un cours seul ------------------------------------
 if ($uri->getOperationType() === RequestUri::OPERATION_ITEM && $uri->getResourceName() === 'courses' && $httpMethod === 'GET') {
     $id = $uri->getIdentifier();
 
@@ -108,5 +109,64 @@ if ($uri->getOperationType() === RequestUri::OPERATION_ITEM && $uri->getResource
     ];
 
     echo json_encode($course);
+    exit;
+}
+
+// --- Modification d'un cours seul ------------------------------------
+if ($uri->getOperationType() === RequestUri::OPERATION_ITEM && $uri->getResourceName() === 'courses' && $httpMethod === 'PUT') {
+    $requestRawContent = file_get_contents('php://input');
+    $arrayContent = json_decode($requestRawContent, true);
+
+    $requiredFields = ['name', 'img', 'video', 'date'];
+    $errors = [];
+    foreach ($requiredFields as $requiredField) {
+        if  (!isset($arrayContent[$requiredField])) {
+            $errors[$requiredField] = "Le champ '$requiredField' est requis";
+        }
+    }
+
+    if (count($errors) > 0) {
+        http_response_code(422); // Unprocessable content
+        echo json_encode($errors);
+        exit;
+    }
+
+    $stmt = $pdo->prepare("UPDATE courses SET course_name=:name, cover_img_url=:img, video_url=:video, date_online=:date WHERE id_course=:id");
+
+    $result = $stmt->execute([
+        'name' => $arrayContent['name'],
+        'img' => $arrayContent['img'],
+        'video' => $arrayContent['video'],
+        'date' => $arrayContent['date'],
+        'id' => $uri->getIdentifier()
+    ]);
+
+    if ($result === false) {
+        http_response_code(500); // Internal Server Error
+        echo json_encode([
+            'error' => "Une erreur est survenue lors de l'enregistrement du nouvel élément"
+        ]);
+        exit;
+    }
+
+    http_response_code(204); // No Content
+    exit;
+}
+
+// --- Suppression d'un cours ------------------------------------------
+if ($uri->getOperationType() === RequestUri::OPERATION_ITEM && $uri->getResourceName() === 'courses' && $httpMethod === 'DELETE') {
+    $stmt = $pdo->prepare('DELETE FROM courses WHERE id_course=:id');
+
+    $result = $stmt->execute(['id' => $uri->getIdentifier()]);
+
+    if ($result === false) {
+        http_response_code(500); // Internal Server Error
+        echo json_encode([
+            'error' => "Une erreur est survenue lors de l'enregistrement du nouvel élément"
+        ]);
+        exit;
+    }
+
+    http_response_code(204); // No Content
     exit;
 }
